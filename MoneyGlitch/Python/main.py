b562b234296e931ec94a6698d6a997808acb89dc
MuffenDuffen <50796186+MuffenDuffen@users.noreply.github.com>
2021-09-20 13:10:26 +0200

    pthn wrking nw

--- MoneyGlitch/Python/main.py
+++ MoneyGlitch/Python/main.py
@@ -1,7 +1,12 @@
 import ctypes
 import os
 
-text = "LOL"
+class cstr (ctypes.Structure):
+    _fields_ = [("str", ctypes.POINTER(ctypes.c_byte)), 
+                ("length", ctypes.c_int)]
+
+
+text = "LOL".encode('utf-8')
 
 path = os.getcwd() #Get Current Path
 
@@ -11,9 +16,17 @@ test = ctypes.CDLL(path) #Load DLL
 
 # making a char array
 arr = ctypes.c_byte * len(text)
-lst = []
-for i in text:
-    lst.append(i)
+
+str = cstr(arr(*text), len(text))
 
 test.print.argtypes = (ctypes.POINTER(ctypes.c_byte), ctypes.c_int)
-test.print(arr(*lst), len(text)) #Call DLL Function
+test.print(str.str, str.length) #Call DLL Function
+
+test.cPrint.argtypes = (ctypes.POINTER(ctypes.c_byte), ctypes.c_int)
+test.cPrint.restype = ctypes.c_void_p
+
+l = cstr.from_address(test.cPrint(str.str, str.length))
+print(l.length)
+
+test.free_cstr(ctypes.byref(l))
+del l
